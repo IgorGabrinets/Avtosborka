18c3212ded5cc392b3f096920546a770347be7ec
Igor Gabrinets <gabrinets1@gmail.com>
2022-12-21 23:27:28 +0100

    First level: level1: Создайте массив размером 100 символов, заполните все ячейки значением 10. 1 и 99 ячейки заполните значениями, равными 0

--- src/main/java/org/Avtosborka/HW20122022.java
+++ src/main/java/org/Avtosborka/HW20122022.java
@@ -6,35 +6,19 @@ package org.Avtosborka;
 
 import java.util.Arrays;
 
+
 public class HW20122022 {
     public static void main(String[] args) {
         int[] mas = new int[100];
-        for (int i = 0; i <= 99; i += 10) {
-            if (mas[0] == 111) ;
-            mas[i] = 111;
-            mas[i + 1] = 111 * 2;
-            mas[i + 2] = 111 * 3;
-            mas[i + 3] = 111 * 4;
-            mas[i + 4] = 111 * 5;
-            mas[i + 5] = 111 * 6;
-            mas[i + 6] = 111 * 7;
-            mas[i + 7] = 111 * 8;
-            mas[i + 8] = 111 * 9;
-            mas[i + 9] = 111 * 10;
-
-
-            System.out.println(mas[i]);
-            System.out.println(mas[i + 1]);
-            System.out.println(mas[i + 2]);
-            System.out.println(mas[i + 3]);
-            System.out.println(mas[i + 4]);
-            System.out.println(mas[i + 5]);
-            System.out.println(mas[i + 6]);
-            System.out.println(mas[i + 7]);
-            System.out.println(mas[i + 8]);
-            System.out.println(mas[i + 9]);
+        for (int i = 0; i <= 9; i += 10) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 1; k <= 10; k++) {
+                    mas[i + j] = 111 * k;
+                    System.out.println(mas[i + j]);
+                }
+            }
         }
-        int s = Arrays.stream(mas).sum();
-        System.out.println("the sum of all elements = " + s);
+        int sum = Arrays.stream(mas).sum();
+        System.out.println("the sum of all elements = " + sum);
     }
 }
